6d9c753eb7f9823ef3233833f1e74204c1ed0686
DonCappuccin0 <doncappuccino@protonmail.com>
2025-02-26 07:46:09 +0100

    Removed correctLetters Set, and improved position checking

--- src/Wordle.java
+++ src/Wordle.java
@@ -11,11 +11,9 @@ public class Wordle {
     private int tries;
     private final String wordToGuess;
     private final Set<Character> availableLetters;
-    private final Set<Character> correctLetters;
 
     private static class GeneratedWord {
         private static final HttpClient httpClient = HttpClient.newHttpClient();
-
         public static String generate(int wordlength) throws URISyntaxException, IOException, InterruptedException {
             String API_URL = "https://random-word-api.vercel.app/api?words=1&length=";
             HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(new URI(API_URL + wordlength)).build();
@@ -27,7 +25,6 @@ public class Wordle {
     public Wordle(int wordLength, int tries) throws URISyntaxException, IOException, InterruptedException {
         this.wordToGuess = Wordle.GeneratedWord.generate(wordLength);
         this.tries = tries;
-        this.correctLetters = new HashSet<>();
         this.availableLetters = new HashSet<>();
 
         for (char c = 'a'; c <= 'z'; c++) {
@@ -70,21 +67,22 @@ public class Wordle {
         for (int i = 0; i < this.wordToGuess.length(); ++i) {
             if (!this.wordToGuess.contains(String.valueOf(playerInput.charAt(i)))) {
                 this.availableLetters.remove(playerInput.charAt(i));
-            } else {
-                this.correctLetters.add(playerInput.charAt(i));
             }
         }
     }
 
     public void positionChecker(String playerInput) {
-        for (int i = 0; i < playerInput.length(); ++i) {
+        for (int i = 0; i < playerInput.length(); i++) {
             char playerInputChar = playerInput.charAt(i);
-            if (!this.correctLetters.contains(playerInputChar)) {
-                System.out.println("The letter " + playerInputChar + " doesn't exist in the word to guess");
-            } else if (this.wordToGuess.indexOf(playerInputChar, i) == playerInput.indexOf(playerInputChar, i)) {
-                System.out.println("The letter " + playerInputChar + " is in the correct place.");
+            int correctIndex = this.wordToGuess.indexOf(playerInputChar, i);
+            if (correctIndex != -1) {
+                if (correctIndex == i) {
+                    System.out.println("The letter " + playerInputChar + " is in the correct place.");
+                } else {
+                    System.out.println("The letter " + playerInputChar + " is not in the correct place.");
+                }
             } else {
-                System.out.println("The letter " + playerInputChar + " is not in the correct place.");
+                System.out.println("The letter " + playerInputChar + " doesn't exist in the word to guess");
             }
         }
     }
